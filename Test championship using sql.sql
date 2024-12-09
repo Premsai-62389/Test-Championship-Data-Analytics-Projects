e5a79d775299d256740bcd5d3c4e4d9ea940f0c4
Premsai <premsaisaiprem9999@gmail.com>
2024-12-09 21:17:06 +0530

    Add files via upload

---
+++ Test championship using sql.sql
@@ -0,0 +1,17 @@
+-- create database testchampionship
+ 
+ -- select * from testchampionship
+ 
+ -- select * from testchampionship where Team="India"
+ 
+ -- select * from testchampionship where Won>=5 and Lost<4
+ 
+ -- SELECT avg(Percentage) as Percentage_of_overall FROM testchampionship 
+
+-- SELECT Team, Won,Lost FROM testchampionship order by Team asc limit 5
+
+-- SELECT Team,Points FROM testchampionship where Points>70
+
+-- SELECT upper(Team),Percentage FROM testchampionship
+
+
